e743aa650749fc1a550add130e06da6d06bf4199
Gabriel Lacerda <gabriellacerdasg@hotmail.com>
2026-09-02 20:12:03 -0300

    atualização comparadores

--- src/dominio/ComparadorContatoPorNome.java
+++ src/dominio/ComparadorContatoPorNome.java
@@ -1,4 +1,11 @@
 package dominio;
 
-public class ComparadorContatoPorNome {
+import java.util.Comparator;
+
+public class ComparadorContatoPorNome implements Comparator<Contato> {
+
+    @Override
+    public int compare(Contato contato1, Contato contato2) {
+        return contato1.getNome().compareTo(contato2.getNome());
+    }
 }
--- src/dominio/Main.java
+++ src/dominio/Main.java
@@ -1,8 +1,41 @@
 package dominio;
 
+import colecao.IColecao;
+import listaencadeada.ListaEncadeada;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Lista ordenada? (S/N)");
+        String resposta = entrada.nextLine();
+
+        boolean ordenada = resposta.equalsIgnoreCase("S");
+
+        IColecao<Contato> contatosPorNome =
+                new ListaEncadeada<Contato>(
+                        new ComparadorContatoPorNome(),
+                        ordenada
+                );
+
+        IColecao<Contato> contatosPorTelefone =
+                new ListaEncadeada<Contato>(
+                        new ComparadorContatoPorTelefone(),
+                        ordenada
+                );
+
+        System.out.println("Lista criada com sucesso.");
+
+        if (ordenada) {
+            System.out.println("Modo: lista ordenada.");
+        } else {
+            System.out.println("Modo: lista desordenada.");
+        }
+
+        entrada.close();
     }
 }
