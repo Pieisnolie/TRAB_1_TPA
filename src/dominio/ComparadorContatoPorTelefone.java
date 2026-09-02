8fd60f18e574dad26863033dfa81dc08ce2633f6
Gabriel Lacerda <gabriellacerdasg@hotmail.com>
2026-09-02 20:12:03 -0300

    atualização comparadores

--- src/dominio/ComparadorContatoPorTelefone.java
+++ src/dominio/ComparadorContatoPorTelefone.java
@@ -1,4 +1,11 @@
 package dominio;
 
-public class ComparadorContatoPorTelefone {
+import java.util.Comparator;
+
+public class ComparadorContatoPorTelefone implements Comparator<Contato> {
+
+    @Override
+    public int compare(Contato contato1, Contato contato2) {
+        return contato1.getTelefone().compareTo(contato2.getTelefone());
+    }
 }
